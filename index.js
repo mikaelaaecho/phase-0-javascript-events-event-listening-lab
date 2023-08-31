a0c019309cee4fdf5e31f7654dc1e18a12ce6239
mikaela echo <mikaelaaecho@gmail.com>
2023-08-30 18:44:12 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+const targetElement = document.getElementById('target');
+
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+  targetElement.textContent = 'Button clicked!';
+});
 }
